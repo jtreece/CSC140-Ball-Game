f99634054c66e5bfdf0f4cd58cc9018de23acbe0
jtreece <joshua_reece5@mymail.eku.edu>
2016-05-07 16:26:14 -0400

    Finished Mechanics and Map
    I implemented shooting mechanics to make the ball shoot at a 45 degree
    angle with respect to the horizontal based on change in mouse position
    (click-and-drag). Collision detection works, but even continuous
    collision detection does not fix involutarily having to shoot twice in
    order to traverse the map. A beginning map is included and works well.
    Camera mechanics could be better but currently follow the ball.

--- CSC140 Final/Assets/PlayerScript.cs
+++ CSC140 Final/Assets/PlayerScript.cs
@@ -2,25 +2,52 @@
 using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
-
-    SpringJoint spring;
+    GameObject map;
     Rigidbody rb;
-    Ray mouse_ray;
-    RaycastHit ray_hit;
+    Vector3 mouse_start_position;
+    bool old_mouse_state;
+    bool moving;
+    float y_rotation;
 
 	// Use this for initialization
-	void Start () {
-	    rb = GetComponent<Rigidbody>();
-        mouse_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-	}
-	
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        map = GameObject.Find("ball_game_map");
+        old_mouse_state = false;
+        y_rotation = 0.0f;
+        //Physics.IgnoreCollision(GetComponent<Collider>(), map.GetComponents<Collider>()[1]);
+    }
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(0) && Physics.Raycast(mouse_ray, out ray_hit, 100.0f))
+        if (Input.GetMouseButtonDown(0) && !old_mouse_state)
         {
             rb.isKinematic = true;
+            old_mouse_state = true;
+            mouse_start_position = Input.mousePosition;
         }
         if (Input.GetMouseButtonUp(0))
+        {
             rb.isKinematic = false;
+            old_mouse_state = false;
+            rb.AddForce(this.transform.forward * (Input.mousePosition - mouse_start_position).magnitude + this.transform.up *  (Input.mousePosition - mouse_start_position).magnitude);
+            moving = true;
+        }
+        if (!old_mouse_state && !moving)
+        {
+            y_rotation += Input.GetAxis("Mouse X") * 0.1f;
+            transform.eulerAngles += new Vector3(0, y_rotation, 0);
+        }
 	}
+
+    void OnCollisionEnter()
+    {
+        rb.isKinematic = true;
+        moving = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        GUI.Label(new Rect(Screen.height - 10, Screen.width-40, 20, 80), "You win!");
+    }
 }
